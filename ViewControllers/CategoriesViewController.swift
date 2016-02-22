d8be969b357b553cdcb06d7e94069191437d87be
Bruno Berisso <bruno@tryolabs.com>
2016-02-22 11:27:10 -0300

    Basic app setup. Add first xib and view controller

---
+++ ViewControllers/CategoriesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CategoriesViewController.swift
+//  SWAPI
+//
+//  Created by Bruno Berisso on 2/22/16.
+//  Copyright © 2016 Bruno Berisso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    
+
+}
